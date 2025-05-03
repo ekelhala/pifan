f7d8b826d25069e9a84ad13ab70016213749e7b0
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-05-03 17:59:20 +0300

    use 0.2 as minimum value

--- main.py
+++ main.py
@@ -16,19 +16,21 @@ def get_cpu_temp():
     return int(temp_str) / 1000
 
 def run():
-    # just a demo, start from 0, increment and decrement
-    value = 0
+    # just a demo, start from 0.0, increment and decrement
+    # values between 0.0 and 0.2 don't do anything to the fan
+    value = 0.0
     while True:
         print(f"current temp:", get_cpu_temp())
-        if value < 100:
-            value += 10
-        else:
-            value = 0
         fan.value = value
+        print(f"current speed {value}")
+        if value < 1:
+            value += 0.2
+        else:
+            value = 0.0
         time.sleep(UPDATE_INTERVAL)
 
 if __name__=="__main__":
     try:
         run()
-    except KeyboardInterrupt:
-        fan.value = 0
+    except Exception:
+        fan.value = 0.0
