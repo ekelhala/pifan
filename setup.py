d0b8d454b7bb549fd36472c5379b38ceef6d1d4f
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-05-04 11:07:03 +0300

    create pifan-module, reorder files, add setup.py and default.conf

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pifan",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pifand = pifan.daemon:run"
+        ]
+    },
+    include_package_data=True,
+    data_files=[
+        ("/etc/pifan", ["./default.conf"]),
+        ("/etc/systemd/system", ["./pifand.service"])
+    ],
+    author="Emil Kelhälä",
+    author_email="emil.kelhala@protonmail.com",
+    license="GPL"
+)
