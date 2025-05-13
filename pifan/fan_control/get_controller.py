f7411130357c6fe2e94a40f15f6eb0215f93bc5c
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-05-13 08:26:43 +0300

    added stepwise controller

--- pifan/fan_control/get_controller.py
+++ pifan/fan_control/get_controller.py
@@ -2,10 +2,11 @@ from pifan.fan_control.base_controller import BaseController, ControllerOptions
 from pifan.fan_control.linear_interpolator_controller import LinearInterpolatorController
 from pifan.fan_control.max_speed_controller import MaxSpeedController
 from pifan.fan_control.silent_controller import SilentController
+from pifan.fan_control.stepwise_controller import StepwiseController
 
 def get_controller(controller_type: str,
                    controller_options: ControllerOptions) -> BaseController:
-    
+
     match controller_type:
         case "linear_interpolator":
             controller = LinearInterpolatorController(controller_options)
@@ -13,6 +14,8 @@ def get_controller(controller_type: str,
             controller = SilentController(controller_options)
         case "max_speed":
             controller = MaxSpeedController(controller_options)
+        case "stepwise":
+            controller = StepwiseController(controller_options)
         case _:
             controller = None
     return controller
---
+++ pifan/fan_control/stepwise_controller.py
@@ -0,0 +1,24 @@
+from pifan.fan_control.base_controller import BaseController
+
+class StepwiseController(BaseController):
+    """
+    This controller adjusts the speed of the fan in increasing steps based on temp_high
+    The steps used:
+    lower than temp_low or <= 29% of temp_high -> 0% speed
+    30-59% of temp_high -> 30% speed
+    60-99% of temp_high -> 60% speed
+    at or over temp_high -> 100% speed
+    """
+
+    def get_speed(self, system_temperature):
+        temp_high_fraction = round(system_temperature / self.options.temp_high, 2)
+        if system_temperature < self.options.temp_low or temp_high_fraction <= 0.29:
+            return 0.0
+        elif temp_high_fraction <= 0.59:
+            return 0.3
+        elif temp_high_fraction <= 0.99:
+            return 0.6
+        return 1.0
+
+    def get_name(self):
+        return "stepwise"
