3c373f9ecbcf291d32e898dc8eb347c4fe32f750
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-05-15 10:37:45 +0300

    stepwise controller: adjust speed based on temp_high - temp_low scale

--- pifan/fan_control/stepwise_controller.py
+++ pifan/fan_control/stepwise_controller.py
@@ -2,21 +2,23 @@ from pifan.fan_control.base_controller import BaseController
 
 class StepwiseController(BaseController):
     """
-    This controller adjusts the speed of the fan in increasing steps based on temp_high
+    This controller adjusts the speed of the fan in steps when it is between temp_low and temp_high
     The steps used:
-    lower than temp_low or <= 29% of temp_high -> 0% speed
-    30-59% of temp_high -> 30% speed
-    60-90% of temp_high -> 60% speed
-    over 90% temp_high -> 100% speed
+    lower than temp_low or < 29% of temp_high-temp_low -> 0% speed
+    30-59% of temp_high-temp_low -> 30% speed
+    60-90% of temp_high-temp_low -> 60% speed
+    over 90% temp_high-temp_low -> 100% speed
     """
 
     def get_speed(self, system_temperature):
-        temp_high_fraction = round(system_temperature / self.options.temp_high, 2)
-        if system_temperature < self.options.temp_low or temp_high_fraction <= 0.29:
+        if system_temperature <= self.options.temp_low:
             return 0.0
-        elif temp_high_fraction <= 0.59:
+        temp_pct = round((system_temperature - self.options.temp_low) / (self.options.temp_high - self.options.temp_low), 2)
+        if temp_pct < 0.3:
+            return 0.0
+        elif temp_pct < 0.6:
             return 0.3
-        elif temp_high_fraction <= 0.90:
+        elif temp_pct < 0.9:
             return 0.6
         return 1.0
 
